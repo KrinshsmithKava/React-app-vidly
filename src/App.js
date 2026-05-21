4ea349e3a5af4d53be1b00a0f55627d35d93e617
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-05-21 14:41:33 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={Rentals} />
+        <Route path="/not-found" component={NotFound} />
       </main>
     );
   }
